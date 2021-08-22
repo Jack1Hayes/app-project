9ccb8193f7c188785c3a03d046d7471e2b035915
Jack Hayes <hayesi@live.com>
2021-08-22 15:48:23 +0200

    Server.js code fix

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-//mongodb
-require('./config/DB');
+// mongodb
+require("./config/db");
 
-const app = require('express')();
+const app = require("express")();
 const port = process.env.PORT || 3000;
-const cors = require('cors');
-app.use(cors);
 
-const UserRouter = require('./api/User')
+//cors
+const cors = require("cors");
+app.use(cors());
 
-//for accepting post form data
+const UserRouter = require("./api/User");
 
-const bodyParser = require('express').json;
+// For accepting post form data
+const bodyParser = require("express").json;
 app.use(bodyParser());
 
-app.use('/user', UserRouter)
+app.use("/user", UserRouter);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
